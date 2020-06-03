64a4ffc9fda46abfa84b5cbb39fec06b1d1e97f9
Patrick Baus <patrick.baus@physik.tu-darmstadt.de>
2020-06-03 14:46:26 +0200

    refactoring

--- php/includes/functions.php
+++ php/includes/functions.php
@@ -12,7 +12,7 @@
  * @return string
  */
 function generateSelect($options = array(), $name = "", $linePrefix = "", $classname = "", $id = "") {
-	$html = $linePrefix . "<select " . (($classname != "") ? "class=\"$classname\"" : "") . (($name != "") ? "name=\"$name\"" : "") . (($id != "") ? "id=\"$id\"" : "") .">\n";
+  $html = $linePrefix . "<select " . (($classname != "") ? "class=\"$classname\"" : "") . (($name != "") ? "name=\"$name\"" : "") . (($id != "") ? "id=\"$id\"" : "") .">\n";
         foreach ( $options as $key => $value ) {
                 $html .= $linePrefix . "\t<option value=\"$key\">$value</option>\n";
         }
@@ -21,64 +21,49 @@ function generateSelect($options = array(), $name = "", $linePrefix = "", $class
 }
 
 function convertTime($time) {
-	$days = floor($time / 1000 / 60 / 600 / 24);
-	$hours = $time / 1000 / 60 / 60 % 24;
-	$mins = $time / 1000 / 60 % 60;
-	$secs = $time / 1000 % 60;
-	$millisecs = $time % 1000;
+  $days = floor($time / 1000 / 60 / 600 / 24);
+  $hours = $time / 1000 / 60 / 60 % 24;
+  $mins = $time / 1000 / 60 % 60;
+  $secs = $time / 1000 % 60;
+  $millisecs = $time % 1000;
 
-	$timeStr = "";
-	$timeStr .= ($days > 0 ? "$days d ": "");
-	$timeStr .= ($hours > 0 ? "$hours h " : "");
-	$timeStr .= ($mins > 0 ? "$mins min " : "");
-	$timeStr .= ($secs > 0 ? "$secs s " : "");
-	$timeStr .= ($millisecs > 0 ? "$millisecs ms " : "");
+  $timeStr = "";
+  $timeStr .= ($days > 0 ? "$days d ": "");
+  $timeStr .= ($hours > 0 ? "$hours h " : "");
+  $timeStr .= ($mins > 0 ? "$mins min " : "");
+  $timeStr .= ($secs > 0 ? "$secs s " : "");
+  $timeStr .= ($millisecs > 0 ? "$millisecs ms " : "");
 
-	return ($timeStr == null ? "0 ms" : trim($timeStr));
+  return ($timeStr == null ? "0 ms" : trim($timeStr));
 }
 
 // Extract all available units from the database
 function getUnits($con) {
-	global $query_get_units;
-	$units = array();
-	$result = $con->query($query_get_units);
-	while($row = $result->fetch()) {
-		$units[$row['id']] = $row['unit'];
-	}
-	return $units;
+  global $query_get_units;
+  return $con->query($query_get_units)->fetchAll(PDO::FETCH_KEY_PAIR);
 }
 
 // Extract all available sensor nodes (Tinkerforge brick daemons) from the database
 function getNodes($con) {
-	global $query_get_nodes;
-	$nodes = array();
-	$result = $con->query($query_get_nodes);
-	while($row = $result->fetch()) {
-		$nodes[$row['id']] = $row['hostname'];
-	}
-	return $nodes;
+  global $query_get_nodes;
+  return $con->query($query_get_nodes)->fetchAll(PDO::FETCH_KEY_PAIR);
 }
 
 // Extract all available rooms
 function getRooms($con) {
-	global $query_get_rooms;
-	$rooms = array();
-	$result = $con->query($query_get_rooms);
-	while($row = $result->fetch()) {
-		$rooms[$row['id']] = ($row['room'] == "" ? "ID: " . $row['id'] : $row['room']);
-	}
-	return $rooms;
+  global $query_get_rooms;
+  return $con->query($query_get_rooms)->fetchAll(PDO::FETCH_KEY_PAIR);
 }
 
 // Get default callback period
 function getDefaultCallbackPeriod($con, $database) {
-	global $query_get_callback_default;
+  global $query_get_callback_default;
   $stmt = $con->query($query_get_callback_default);
-	if (!$stmt) {
-		printf('Binding output parameters failed for query "%s": (%d) %s' . PHP_EOL, $query_get_callback_default, $stmt->errno, $stmt->error);
-	}
+  if (!$stmt) {
+    printf('Binding output parameters failed for query "%s": (%d) %s' . PHP_EOL, $query_get_callback_default, $stmt->errno, $stmt->error);
+  }
 
-	return $stmt->fetchColumn();
+  return $stmt->fetchColumn();
 }
 
 // Get the default port number of the sensor nodes
